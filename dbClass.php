ea0770892017624e742784caf9b207f53d30950f
FaridaHesham99 <faridamohamed1802642@miuegypt.edu.eg>
2021-06-04 01:58:07 +0200

    Create dbClass.php

---
+++ dbClass.php
@@ -0,0 +1,15 @@
+<?php
+class db{
+  private $host="localhost";
+  private $user="root";
+  private $pass="";
+  private $dbName="coffee_shop";
+
+  protected function connect(){
+    $conn = 'mysql:host='.$this->host.';dbname='.$this->dbName;
+    $pdo = new PDO($dsn,$this->user,$this->pass);
+    $pdo ->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    return $pdo;
+  }
+}
+ ?>
