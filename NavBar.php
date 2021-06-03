0d737e6be21af974e9183060912c841879c96aea
SamihaHussein2 <74453015+SamihaHussein2@users.noreply.github.com>
2021-06-03 19:29:22 +0200

    Add files via upload

---
+++ NavBar.php
@@ -0,0 +1,23 @@
+<html>
+
+<head>
+
+</head>
+
+<body>
+
+<h2>Menu</h2>
+     <div class="divMenu" >
+     
+     <a href="viewMenu.php" > Home </a> 
+     <a href="Registration/SignUp.php" > Register </a>
+     <a href="login.php" > Login </a>
+     
+     </div>
+
+</body>
+
+</html>
+
+
+
