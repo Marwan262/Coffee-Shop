47d409175bd95009c871190d1830d806a9937ad7
yaraamrsalah <yaraamrsalahh@gmail.com>
2021-06-03 21:58:57 +0200

    Created Nav Bar

---
+++ NavBar/navBar.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+<link rel="stylesheet" href="navBar.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+<div class="topnav">
+  <a class="active" href="/Coffee-Shop/viewMenu.php">Menu</a>
+    <div class="topnav-right"> 
+        <a href="/Coffee-Shop/login.php">Login</a> <!-- Remove after Login -->
+        <a href="/Coffee-Shop/Registration/SignUp.php">Register</a> <!-- Remove after Login -->
+        <a href="">Modify Inventory</a> <!--After employee login--> 
+        <a href="">View Inventory</a> <!--After employee login--> 
+        <a href="">Modify Users</a> <!--After employee login--> 
+        <a href="">View Users</a> <!--After employee login--> 
+        <a href=""><i class="fa fa-shopping-cart"></i> Cart</a> <!--After customer login--> 
+        <a href="">Profile</a> <!-- After any user login--->
+        <a href="">Logout</a> <!-- After any user login -->
+   </div>
+</div>
+
+
+
+</body>
+</html>
