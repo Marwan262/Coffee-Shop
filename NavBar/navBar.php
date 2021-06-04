a9d2cbbfa3db5badda3b8a213e6cfc6b403ba833
FaridaHesham99 <faridamohamed1802642@miuegypt.edu.eg>
2021-06-04 20:06:37 +0200

    Update navBar.php

--- NavBar/navBar.php
+++ NavBar/navBar.php
@@ -1,19 +1,33 @@
 <html>
 <head>
-<link rel="stylesheet" href="NavBar/navBar.css">
+  <?php
+  ob_start();
+  include "../login.php";
+  $data = ob_get_clean();
+  $name=$_SESSION["username"];
+   ?>
+<link rel="stylesheet" href="../NavBar/navBar.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
 <div class="topnav">
   <a class="active" href="/Coffee-Shop/viewMenu.php">Menu</a>
-    <div class="topnav-right"> 
-        <a href="/Coffee-Shop/login.php">Login</a> <!-- Remove after Login -->
-        <a href="/Coffee-Shop/Registration/SignUp.php">Register</a> <!-- Remove after Login -->
-        <a href="/Coffee-Shop/inventory.php">View Inventory</a> <!--After employee login--> 
-        <a href="/Coffee-Shop/users.php">View Users</a> <!--After employee login--> 
-        <a href=""><i class="fa fa-shopping-cart"></i> Cart</a> <!--After customer login--> 
-        <a href="">Profile</a> <!-- After any user login--->
-        <a href="/Coffee-Shop/php/logout.php">Logout</a> <!-- After any user login -->
+    <div class="topnav-right">
+          <?php
+          if (empty($_SESSION['username'])){
+            echo '<a href="/Coffee-Shop/login.php">Login</a>
+                  <a href="/Coffee-Shop/Registration/SignUp.php">Register</a>';
+          }
+
+          else if (isset($_SESSION['username'])) {
+            echo '<a href=""><i class="fa fa-shopping-cart"></i> Cart</a>
+                  <a href="/Coffee-Shop/profile.php">Profile</a>
+                  <a href="/Coffee-Shop/php/viewOrders.php">Order History</a>
+                  <a href="/Coffee-Shop/php/logout.php">Logout</a>';
+          }
+           ?>
+
+
    </div>
 </div>
 
