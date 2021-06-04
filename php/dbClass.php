2645ad80aa60723f7caaf63e4ba5c6d47021097e
FaridaHesham99 <faridamohamed1802642@miuegypt.edu.eg>
2021-06-04 02:49:00 +0200

    Update dbClass.php

--- php/dbClass.php
+++ php/dbClass.php
@@ -7,7 +7,7 @@ class db{
 
   protected function connect(){
     $conn = 'mysql:host='.$this->host.';dbname='.$this->dbName;
-    $pdo = new PDO($dsn,$this->user,$this->pass);
+    $pdo = new PDO($conn,$this->user,$this->pass);
     $pdo ->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     return $pdo;
   }
