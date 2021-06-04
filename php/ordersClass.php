36b93d6aefaffb7c1353b1f341acaadadce7b56d
FaridaHesham99 <faridamohamed1802642@miuegypt.edu.eg>
2021-06-04 13:36:55 +0200

    Update ordersClass.php

--- php/ordersClass.php
+++ php/ordersClass.php
@@ -5,22 +5,34 @@ class orders extends db{
   public function getOrders(){
     $sql="SELECT * FROM drinkorder";
     $result=$this->connect()->query($sql);
-    while ($row=$result->fetch()) {
-      $drink=$row['Drink'];
-      $size=$row['Size'];
-      $condiments=$row['Condiments'];
-      $milkType=$row['MilkType'];
-      $total=$row['TotalPrice'];
 
-      $sql ="SELECT drinkName FROM drink WHERE id='$drink' ";
-      $result = $this->connect()->query($sql);
-      echo $row['drinkName'];
+  echo "<table class='table table-striped'>
+  <thead>
+  <tr>
+  <th>Drink</th>
+  <th>Size</th>
+  <th>Condiments</th>
+  <th>Milk Type</th>
+  <th>Total Price</th>
+  </tr>
+  </thead>";
+  while ($row=$result->fetch()) {
+    $drink=$row['Drink'];
+    $size=$row['Size'];
+    $condiments=$row['Condiments'];
+    $milkType=$row['MilkType'];
+    $total=$row['TotalPrice'];
 
-      $sql ="SELECT Size FROM size WHERE id='$size' ";
-      $result = $this->connect()->query($sql);
-      echo $row['Size'];
-
+    $sql ="SELECT drinkName FROM drink WHERE id='$drink' ";
+    $result = $this->connect()->query($sql);
+    while ($row2=$result->fetch()){
+      echo "<tbody><tr><td>".$row2['drinkName']."</td></tr></tbody>" ;
     }
+
+
+  }
+echo "</table>";
+
   }
 }
  ?>
