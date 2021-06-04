16fa52c329454d3e80da192fa51a3b5808238c26
marwan hashem <marawan.hashem@gmail.com>
2021-06-04 18:05:01 +0200

    new cart

--- viewMenu.php
+++ viewMenu.php
@@ -1,60 +1,76 @@
 <?php
 include 'php/classes.php';
-include "NavBar.php";
-
 if (!isset($_SESSION['Cart'])) {
   $_SESSION['Cart'] = "";
 }
-$conn = new mysqli("localhost", "root", "", "coffee_shop");
-$sql1 = "SELECT * FROM drink";
-$sql2 = "SELECT * FROM condiments";
-$sql3 = "SELECT * FROM milktype";
-$result1 = mysqli_query($conn,$sql1);
-$result2 = mysqli_query($conn,$sql2);
-$result3 = mysqli_query($conn,$sql3);
+$conn = new Database_Handler();
+// $conn = new mysqli("localhost", "root", "", "c2");
+// $sql1 = "SELECT * FROM drink";
+// $sql2 = "SELECT * FROM condiments";
+// $sql3 = "SELECT * FROM milktype";
+// $result1 = mysqli_query($conn,$sql1);
+// $result2 = mysqli_query($conn,$sql2);
+// $result3 = mysqli_query($conn,$sql3);
  ?>
  <!DOCTYPE html>
  <html lang="en" dir="ltr">
    <head>
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
      <meta charset="utf-8">
      <title>Menu</title>
      <script type="text/javascript">
-       function addAnotherDrink(id){
-         var checked = false;
+       function addAnotherDrink(){
+         var checked1 = false;
+         var checked2 = false;
+         var checked3 = false;
          var order = "";
-         var drink,milk,condiment = " ";
+         var condiments = " ";
+         var drink,milk,size = " ";
          var array1 = document.getElementsByClassName("drink");
          var array2 = document.getElementsByClassName("condiment");
          var array3 = document.getElementsByClassName("milk");
+         var array4 = document.getElementsByClassName("size");
 
          for (var i = 0; i < array1.length; i++) {
            if (array1[i].checked == true) {
               drink = array1[i];
-              checked = true;
+              checked1 = true;
            }
          }
          for (var i = 0; i < array2.length; i++) {
            if (array2[i].checked == true) {
-              condiment = array2[i];
+             condiments += array2[i].value + "/";
            }
          }
+         condiments = condiments.substring(0,condiments.length-1);
          for (var i = 0; i < array3.length; i++) {
            if (array3[i].checked == true) {
               milk = array3[i];
+              checked2 = true;
+           }
+         }
+         for (var i = 0; i < array4.length; i++) {
+           if (array4[i].checked == true) {
+              size = array4[i];
+              checked3 = true;
            }
          }
 
-        if (!checked) {
-          errorMessage();
+        if (!checked1 || !checked2 || !checked3) {
+          alert("Please finish your order.");
           return;
         }
-         order = drink.value + "," + condiment.value + "," + milk.value;
          var formData = new FormData();
-         formData.append('order', order);
-         // alert(order);
+         // alert(condiments);
+         formData.append('condiments', condiments);
+         formData.append('milk', milk.value);
+         formData.append('size', size.value);
+         formData.append('drink', drink.value);
+         formData.append('q', 'push');
          var xmlhttp=new XMLHttpRequest();
          xmlhttp.onreadystatechange=function() {
            if (this.readyState==4 && this.status==200) {
+             document.getElementById("d").innerHTML=this.responseText;
              clear();
            }
          }
@@ -65,104 +81,92 @@ $result3 = mysqli_query($conn,$sql3);
          var array1 = document.getElementsByClassName("drink");
          var array2 = document.getElementsByClassName("condiment");
          var array3 = document.getElementsByClassName("milk");
+         var array4 = document.getElementsByClassName("size");
          for (var i = 0; i < array1.length; i++) {
            array1[i].checked = false;
          }
-         for (var j = 0; j < array2.length; j++) {
-           array2[j].checked = false;
+         for (var i = 0; i < array2.length; i++) {
+           array2[i].checked = false;
          }
-         for (var k = 0; k < array3.length; k++) {
-           array3[k].checked = false;
+         for (var i = 0; i < array3.length; i++) {
+           array3[i].checked = false;
          }
-       }
-       function errorMessage(){
-         alert("Please finish your order.")
+         for (var i = 0; i < array4.length; i++) {
+           array4[i].checked = false;
+         }
        }
        function checkout(){
-         window.location.href = "tempcart.php";
+         window.location.href = "cart.php";
        }
      </script>
      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 
    </head>
    <body>
-     <h2>Menu</h2>
-     <form method="post" action="" name="form">
-        <h4>Drinks</h4>
-        <table class="table" width="30%" border="1" style="border-collapse:collapse; margin-top:4px;">
-             <thead>
-               <tr>
-                 <th><strong></strong></th>
-                 <th><strong>Drink</strong></th>
-                 <th><strong>Price</strong></th>
-                 <th><strong>Description</strong></th>
-                 <th><strong></strong></th>
-               </tr>
-             </thead>
-             <tbody>
-               <?php
-               while ($row1 = mysqli_fetch_array($result1)) {
-                 echo "<tr>";
-                 echo "<td><img src='pics/" . $row1['drinkName'] . ".jpg' alt='Italian Trulli' width = '75' height = '75'></td>";
-                 echo "<td>" . $row1['drinkName'] . "</td>";
-                 echo "<td>" . $row1['Price'] . "EGP</td>";
-                 echo "<td>" . $row1['Description'] . "</td>";
-                 echo "<td><input type='radio' class = 'drink' id='drink' name='drink' value='" . $row1['drinkName'] . "'></td>";
-                 echo "</tr>";
-               }
-                ?>
-             </tbody>
+     <p id = "d"></p>
+     <h1>Menu</h1>
+     <form method="post" action="">
+     <div class="container">
+        <h2>Drinks</h2>
+        <table class="table">
+            <thead>
+                <th>Drink Name</th>
+                <th>Price</th>
+                <th>Description</th>
+                <th></th>
+            </thead>
+            <tbody>
+            <?php $conn->displayDrink(); ?>
+            </tbody>
         </table>
-     </form>
-     <div class="">
-        <h4>Milk</h4>
-        <table class="table"  border="1" style="border-collapse:collapse; margin-top:4px;">
-             <thead>
-               <tr>
-                 <th><strong>Milk Type</strong></th>
-                 <th><strong>Price</strong></th>
-                 <th><strong></strong></th>
-               </tr>
-             </thead>
-             <tbody>
-               <?php
-               while ($row3 = mysqli_fetch_array($result3)) {
-                 echo "<tr>";
-                 echo "<td>" . $row3['Type'] . "</td>";
-                 echo "<td>" . $row3['Price'] . "EGP</td>";
-                 echo "<td><input type='radio' class = 'menu' id='milk' name='milk' value='" . $row3['Type'] . "'></td>";
-                 echo "</tr>";
-               }
-                ?>
-             </tbody>
-        </table>
+<table>
+
+</table>
      </div>
-     <div class="">
-        <h4>Condiments</h4>
-        <table class="table" width="30%" border="1" style="border-collapse:collapse; margin-top:4px;">
-             <thead>
-               <tr>
-                 <th><strong>Condiment</strong></th>
-                 <th><strong>Price</strong></th>
-                 <th><strong></strong></th>
-               </tr>
-             </thead>
-             <tbody>
-               <?php
-               while ($row2 = mysqli_fetch_array($result2)) {
-                 echo "<tr>";
-                 echo "<td>" . $row2['Name'] . "</td>";
-                 echo "<td>" . $row2['Price'] . "EGP</td>";
-                 echo "<td><input type='checkbox' class = 'menu' id='condiment' name='condiment' value='" . $row2['Name'] . "'></td>";
-                 echo "</tr>";
-               }
-                ?>
-             </tbody>
+     <div class="container">
+     <h2>Choose Your Size</h2>
+        <table class="table">
+            <thead>
+                <th>Size</th>
+                <th>Price</th>
+                <th></th>
+            </thead>
+            <tbody>
+            <?php $conn->displaySize(); ?>
+            </tbody>
+        </table>
+
+     </div>
+     <div class="container">
+     <h2>Choose Your Milk</h2>
+        <table class="table">
+            <thead>
+                <th>Milk Type</th>
+                <th>Price</th>
+                <th></th>
+            </thead>
+            <tbody>
+            <?php $conn->displayMilkTypes();?>
+            </tbody>
+        </table>
+
+     </div>
+     <div class="container">
+     <h2>Extras</h2>
+        <table class="table">
+            <thead>
+                <th>Name</th>
+                <th>Price</th>
+                <th></th>
+            </thead>
+            <tbody>
+            <?php $conn->displayCondiments();?>
+            </tbody>
         </table>
      </div>
 
-       <input type="button" name="" value="Add Another Drink" onclick="addAnotherDrink(1)">
-       <input type="button" name="" value="Check out" onclick="addAnotherDrink(2)">
+       <input type="button" name="" value="Add Another Drink" onclick="addAnotherDrink()">
+       <input type="button" name="" value="Check out" onclick="checkout()">
        </form>
    </body>
  </html>
